9960422504910d16b80ba752399504a09a4b0273
Ariful <arifsofg@gmail.com>
2018-12-26 23:11:46 +0600

    python comments

--- py.py
+++ py.py
@@ -1,17 +1,9 @@
-#translate function
+#python comments
 
-#to be or not to be
-def translate(phrase):
-	translation = ""
-	for letter in phrase:
-		if letter.lower() in "aeiou":
-			if letter.isupper():
-				translation = translation + "G"
-			else: 
-				translation = translation + "g"	
-		else:
-			translation = translation + letter
-	return translation
-	
-print(translate(input("Enter a phrase ")))	
-
+#this is a comments
+'''
+ more comments here
+ invidual lines of comments
+ 
+'''
+print("comments are fun")
