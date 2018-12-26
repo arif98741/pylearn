2c2f384ede4c6eae35d7574acbd308acf5e3da93
Ariful <arifsofg@gmail.com>
2018-12-26 23:36:32 +0600

    readfile by for loop all lines

--- py.py
+++ py.py
@@ -1,5 +1,10 @@
 #python reading a file
 
 employee_file = open("employees.txt", "r")
-print(employee_file.readline())
+#print(employee_file.readlines())
+#print(employee_file.readline())
+employees = employee_file.readlines()
+
+for index in range(len(employees)):
+	print(employees[index])
 employee_file.close()
