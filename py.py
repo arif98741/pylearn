7fd49b148465ea84e3b7b919f66e70cc70d6a87e
Ariful <arifsofg@gmail.com>
2018-12-27 00:38:46 +0600

    Quiz test by python code

--- py.py
+++ py.py
@@ -1,10 +1,23 @@
-from  Student import  Student
+from Question import Question
 
-student1 = Student("jim","Business","3.11",False)
-student2 = Student("Pam","Art",4.00,False)
-print(student2.name)
-print(student2.major)
-print(student2.gpa)
+question_prompts = [
+	"what color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+	"what color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+	"what color are straberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+]
 
+questions = [
+	Question(question_prompts[0],"a"),
+	Question(question_prompts[1],"2"),
+	Question(question_prompts[2],"b"),
+]
 
+def runTest(questions):
+	score = 0
+	for question in questions:
+		answer = input(question.prompt)
+		if answer == question.answer:
+			score += 1
+	print("You got "+str(score)+"/ "+str(len(questions)) + "correct")		
 
+runTest(questions)
