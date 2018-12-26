7fb36cc2a50faa20a8f244a2e19a5f72361d0fd4
Ariful <arifsofg@gmail.com>
2018-12-27 00:18:01 +0600

    Python class and objects usage

--- py.py
+++ py.py
@@ -1,4 +1,10 @@
-#python modules
-import  useful_tools
+from  Student import  Student
 
-print(useful_tools.roll_dice(10))
+student1 = Student("jim","Business","3.11",False)
+student2 = Student("Pam","Art",4.00,False)
+print(student2.name)
+print(student2.major)
+print(student2.gpa)
+
+
+
