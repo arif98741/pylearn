7d6fa18ecf46aae0834b19aa98f17700bb9739b1
Ariful <arifsofg@gmail.com>
2018-12-26 23:05:10 +0600

    translate function added

--- py.py
+++ py.py
@@ -1,12 +1,14 @@
-#2D list and nested loops
-number_grid = [
-	[2, 3, 4],
-	[5, 6, 7],
-	[8, 9, 10],
-	[0]
-]
+#translate function
 
-for row in number_grid:
-	for col in row:
-		print(col)
+#to be or not to be
+def translate(phrase):
+	translation = ""
+	for letter in phrase:
+		if letter in "AEIOUaeiou":
+			translation = translation + "g"
+		else:
+			translation = translation + letter
+	return translation
 	
+print(translate(input("Enter a phrase ")))	
+
